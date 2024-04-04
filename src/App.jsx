fc1561f11c3d9cc84f2527a133f8421aa93b353b
Kyle <kylezhengkz@gmail.com>
2024-04-03 22:46:09 -0400

    .

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="label"></input>
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="btn btn-danger"></button>
+        </li>
+      </ul>
+    </>
+  );
+}
